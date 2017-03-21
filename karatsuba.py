f6f12825af549b86568c86b04bafa255ec5d328b
ravisponnoju <ravisponnoju@gmail.com>
2017-03-21 12:12:26 -0500

    karatsuba algo implementation in python

---
+++ karatsuba.py
@@ -0,0 +1,49 @@
+
+import timeit
+
+start = timeit.default_timer()
+
+
+def product(p, q):
+
+    # base case to return product of two
+    # single digit integers
+    if p < 10 and q < 10:
+        return p*q
+
+    else:
+        l1 = len(str(p))
+        l2 = len(str(q))
+
+        # calculate m from the bigger number
+        # to calculate product of two integers as
+        # per formula; 10^2m (ac) + 10^m ((a+b)(c+d) -ac -bd) + bd
+        if l1 > l2:
+            m = (l1 / 2) + (l1 % 2)
+        else:
+            m = (l2 / 2) + (l2 % 2)
+
+        # splitting the given integer into two parts
+        a, b = divmod(p, 10 ** m)
+        c, d = divmod(q, 10 ** m)
+
+        # printing the new numbers created by splitting the
+        # original number into two parts
+        # print a, b, c, d
+        z1 = product(a, c)
+        z3 = product(b, d)
+        z2 = product(a+b, c+d) - z1 - z3
+
+        # printing the intermediate products calculated
+        # print "z1, z2, z3, m", z1, z2, z3, m
+
+        return (10 ** (2*m)) * z1 + (10 ** m) * z2 + z3
+
+x = 3141592653589793238462643383279502884197169399375105820974944592
+y = 2718281828459045235360287471352662497757247093699959574966967627
+
+print product(x, y)
+
+stop = timeit.default_timer()
+
+print stop - start
