a696e7d8da11533b0871290f273a33e1b49900ad
sun-mylove <ravisponnoju@gmail.com>
2018-07-18 01:26:57 -0500

    for readability...

--- algorithms/number-theory/fibonacci_dp.py
+++ algorithms/number-theory/fibonacci_dp.py
@@ -4,7 +4,6 @@ start = timeit.default_timer()
 
 
 def fibonacci_dp(n):
-    global fib_dict
 
     try:
         return fib_dict[n]
@@ -12,14 +11,13 @@ def fibonacci_dp(n):
     except KeyError:
 
         if n == 1 or n == 2:
-            f = 1
+            fib_dict[n] = 1
         else:
-            f = fibonacci_dp(n - 1) + fibonacci_dp(n - 2)
+            fib_dict[n] = fibonacci_dp(n - 1) + fibonacci_dp(n - 2)
 
-        fib_dict[n] = f
         return fib_dict[n]
 
-fib_dict = {}
+global fib_dict = {}
 print fibonacci_dp(35)
 
 print timeit.default_timer() - start
