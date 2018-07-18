16ab92240140ea9e51d25c9364e8d37421496edd
sun-mylove <ravisponnoju@gmail.com>
2018-07-18 01:42:18 -0500

    minor fix...

--- algorithms/number-theory/fibonacci_dp.py
+++ algorithms/number-theory/fibonacci_dp.py
@@ -4,12 +4,10 @@ start = timeit.default_timer()
 
 
 def fibonacci_dp(n):
-
     try:
         return fib_dict[n]
 
     except KeyError:
-
         if n == 1 or n == 2:
             fib_dict[n] = 1
         else:
@@ -17,7 +15,9 @@ def fibonacci_dp(n):
 
         return fib_dict[n]
 
-global fib_dict = {}
+global fib_dict
+fib_dict = {}
+
 print fibonacci_dp(35)
 
 print timeit.default_timer() - start
